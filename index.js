be2b0d0540a379ae03c1b563fd5cba34dd75e0b6
Carmela Torres Molleda <carmela.torres@wework.com>
2018-09-04 19:45:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 document.addEventListener("DOMContentloaded", (e) => {
+  function replaceMovieContent(movie){
+  document.getElementById("title").innerHTML = movies[movie].title;
+ 
+  document.getElementById("director").innerHTML =
+  movies[movie].director;
   
+  document.getElementById("genre").innerHTML =
+  movies[movie].genre;
+  
+  document.getElementById("filmRating").innerHTML =
+  movies[movie].filmRating;
+  
+  document.getElementById("poster").src =
+  movies[movie].poster;
+  
+  document.getElementById("audienceScore").innerHTML =
+  movies[movie].audienceScore;
+  
+  document.getElementById("description").innerHTML =
+  movies[movie].description;
   // invoking a function here will make sure it happens on page load
   // your code here
 });
