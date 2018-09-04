1a1d2a70034acd1d43b9cbef7eb86e474cd29f68
Carmela Torres Molleda <carmela.torres@wework.com>
2018-09-04 19:46:51 +0000

    automatically backed up by learn

--- test/helpers.js
+++ test/helpers.js
@@ -1,10 +1,10 @@
-const chai = require('chai')
-global.expect = chai.expect
-const fs = require('file-system')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
-const babel = require('babel-core');
-const sinon = require('sinon')
+//const chai = require('chai')
+//global.expect = chai.expect
+//const fs = require('file-system')
+//const jsdom = require('mocha-jsdom')
+//const path = require('path')
+//const babel = require('babel-core');
+//const sinon = require('sinon')
 
 const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
 
