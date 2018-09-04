e8b704bbc049f1a2a9f12abf212bdfc227fd663e
Carmela Torres Molleda <carmela.torres@wework.com>
2018-09-04 19:46:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,31 @@ document.addEventListener("DOMContentloaded", (e) => {
   
   document.getElementById("description").innerHTML =
   movies[movie].description;
+   // let cast = document.getElementById("cast");
+    let movieCast = '';
+  for (let index=0;index<movies[movie].cast.length;index++){
+   movieCast=movieCast+movies[movie].cast[index].role + movies[movie].cast[index].actor;
+  }
+  document.getElementById("cast").innerHTML =movieCast;
+   movies[movie].cast;
+  let movieReviews = '';
+ for (let index=0; index< movies[movie].reviews.length;index++) {
+  movieReviews = movieReviews+movies[movie].reviews[index].username + movies[movie].reviews[index].content;
+ }
+ document.getElementById("reviews").innerHTML=movieReviews; 
+ 
+ 
+ }
+ document.addEventListener("DOMContentLoaded", (e) => {
   // invoking a function here will make sure it happens on page load
   // your code here
+  document.getElementById("Titanic").addEventListener("click", function(e) {
+      replaceMovieContent("Titanic");
+    }
+  );
+  document.getElementById("Terminator 2").addEventListener("click",function(e) {
+      replaceMovieContent("Terminator 2");
+      
+   });
 });
+  
