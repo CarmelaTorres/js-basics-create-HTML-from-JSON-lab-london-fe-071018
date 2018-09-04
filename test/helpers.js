3b32a62c96bf6e33ae681d12427e3d547fcbc6db
Carmela Torres Molleda <carmela.torres@wework.com>
2018-09-04 19:47:55 +0000

    automatically backed up by learn

--- test/helpers.js
+++ test/helpers.js
@@ -6,22 +6,22 @@
 //const babel = require('babel-core');
 //const sinon = require('sinon')
 
-const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
+//const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
 
-const babelResult = babel.transformFileSync(
+c//onst babelResult = babel.transformFileSync(
   path.resolve(__dirname, '..', 'index.js'), {
     presets: ['env']
   }
 );
 //
-const jsonResult = babel.transformFileSync(
+//const jsonResult = babel.transformFileSync(
   path.resolve(__dirname, '..', 'movies.json'), {
     presets: ['env']
   }
 );
 //
 
-const jsonFix = jsonResult.code.slice(0,15)+"var "+jsonResult.code.slice(15)
+//const jsonFix = jsonResult.code.slice(0,15)+"var "+jsonResult.code.slice(15)
 const src = jsonFix + babelResult.code
 
 jsdom({
